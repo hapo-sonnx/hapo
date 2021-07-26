541c2557eafcb800dc4f2312346936dfafb59b5b
son19999 <son.nx@haposoft.com>
2021-07-26 10:52:31 +0700

    add mess and login

--- javascript/popover.js
+++ javascript/popover.js
@@ -1,3 +1,13 @@
-$(function () {
-  $('[data-toggle="popover"]').popover()
-})
+  $(function () {
+    $('[data-toggle="popover"]').popover()
+  })
+
+  $(document).ready(function(){
+    $(".close-button").click(function(){
+      $(".text-mess").hide();
+    });
+    $(".img-mess").click(function(){
+      $(".text-mess").show();
+    });
+  });
+
